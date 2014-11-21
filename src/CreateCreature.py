32351182c63716307f6c38fa8ac4340628c0aba5
Daniel Lamothe <dar.lamothe@gmail.com>
2014-11-21 11:57:56 -0500

    Created Creature

--- src/CreateCreature.py
+++ src/CreateCreature.py
@@ -1,101 +1,91 @@
 __author__ = 'User'
 
 import Action
+import Creature
 
-my_dict = {}
-my_dict.__setitem__('name', '')
-my_dict.__setitem__('size', '')
-my_dict.__setitem__('specification', '')
-my_dict.__setitem__('alignment', '')
-my_dict.__setitem__('ac', 0)
-my_dict.__setitem__('hp', '')
-my_dict.__setitem__('speed', '')
-my_dict.__setitem__('attribute_str', 0)
-my_dict.__setitem__('attribute_dex', 0)
-my_dict.__setitem__('attribute_con', 0)
-my_dict.__setitem__('attribute_int', 0)
-my_dict.__setitem__('attribute_wis', 0)
-my_dict.__setitem__('attribute_cha', 0)
-my_dict.__setitem__('senses', {'DarkVision': '', 'TremorSense': '', 'BlindSense': ''})
-my_dict.__setitem__('languages', '')
-my_dict.__setitem__('challenge_rating', '')
-my_dict.__setitem__('action_set', []) #list of actions...
 
-print('You will be prompted for values representing a creature. Press X to exit at any time')
-selection = ''
-while selection != 'x':
-    for item in my_dict:
-        selection = input('Name:')
-        my_dict.__setitem__('name', selection)
+def prompt_create(self):
+    selection = ''
+    attributes = {}
+    senses = {}
+    action_set = []
 
-        selection = input('Size:')
-        my_dict.__setitem__('size', selection)
+    print('You will be prompted for values representing a creature. Press X to exit at any time')
+    while selection != 'x':
+            selection = input('Name:')
+            name = selection
 
-        selection = input('Specification:')
-        my_dict.__setitem__('specification', selection)
+            selection = input('Size:')
+            size = selection
 
-        selection = input('Alignment:')
-        my_dict.__setitem__('alignment', selection)
+            selection = input('Specification:')
+            specification = selection
 
-        selection = input('Armor Class (int):')
-        my_dict.__setitem__('ac', selection)
+            selection = input('Alignment:')
+            alignment = selection
 
-        selection = input('Hit Points:')
-        my_dict.__setitem__('hp', selection)
+            selection = input('Armor Class (int):')
+            ac = selection
 
-        selection = input('Speed (feet):')
-        my_dict.__setitem__('speed', selection)
+            selection = input('Hit Points:')
+            hp = selection
 
-        selection = input('Strength (Int)')
-        my_dict.__setitem__('attribute_str', selection)
+            selection = input('Speed (feet):')
+            speed = selection
 
-        selection = input('Dexterity (Int)')
-        my_dict.__setitem__('attribute_dex', selection)
+            selection = input('Strength (Int)')
+            attributes.__setitem__('STR', selection)
 
-        selection = input('Constitution (Int)')
-        my_dict.__setitem__('attribute_con', selection)
+            selection = input('Dexterity (Int)')
+            attributes.__setitem__('DEX', selection)
 
-        selection = input('Intelligence (Int)')
-        my_dict.__setitem__('attribute_int', selection)
+            selection = input('Constitution (Int)')
+            attributes.__setitem__('CON', selection)
 
-        selection = input('Wisdom (Wis)')
-        my_dict.__setitem__('attribute_wis', selection)
+            selection = input('Intelligence (Int)')
+            attributes.__setitem__('INT', selection)
 
-        selection = input('Charisma (Int)')
-        my_dict.__setitem__('attribute_cha', selection)
+            selection = input('Wisdom (Wis)')
+            attributes.__setitem__('WIS', selection)
 
-        selection = input('Senses: DarkVision?')
-        my_dict.__setitem__('senses'['DarkVision'], selection)
+            selection = input('Charisma (Int)')
+            attributes.__setitem__('CHA', selection)
 
-        selection = input('Senses: TremorSense?')
-        my_dict.__setitem__('senses'['TremorSense'], selection)
+            selection = input('Senses: DarkVision?')
+            senses.__setitem__('DarkVision', selection)
 
-        selection = input('Senses: BlindSense?')
-        my_dict.__setitem__('senses'['BlindSense'], selection)
+            selection = input('Senses: TremorSense?')
+            senses.__setitem__('TremorSense', selection)
 
-        selection = input('Languages:')
-        my_dict.__setitem__('languages', selection)
+            selection = input('Senses: BlindSense?')
+            senses.__setitem__('BlindSense', selection)
 
-        selection = input('Challenge Rating:')
-        my_dict.__setitem__('challenge_rating', selection)
+            selection = input('Languages:')
+            languages = selection
 
-        # Build an Action
-        action_name = ''
-        action_desc = ''
-        action_attack = ''
-        action_hit = ''
+            selection = input('Challenge Rating:')
+            challenge_rating = selection
 
-        selection = input('Action Name:')
-        action_name = selection
+            # Build an Action
+            selection = input('Action Name:')
+            action_name = selection
 
-        selection = input('Action Descriptor:')
-        action_desc = selection
+            selection = input('Action Descriptor:')
+            action_desc = selection
 
-        selection = input('Attack Modifier to Hit:')
-        action_attack = selection
+            selection = input('Attack Modifier to Hit:')
+            action_attack = selection
 
-        selection = input('Attack Damage:')
-        action_hit = selection
+            selection = input('Attack Damage:')
+            action_hit = selection
 
-        creature_action = Action(action_name, action_desc, action_attack, action_hit)
-        my_dict.__setitem__('action_set', [].append(creature_action))
+            creature_action = Action(action_name, action_desc, action_attack, action_hit)
+            assert isinstance(creature_action, Action)
+            action_set.append(creature_action)
+
+            # Create the Creature
+            created_creature = Creature(name, size, specification, alignment, ac, hp, speed, attributes, senses, languages, challenge_rating, action_set)
+
+            assert isinstance(created_creature, Creature)
+            selection = 'x'
+    return created_creature
